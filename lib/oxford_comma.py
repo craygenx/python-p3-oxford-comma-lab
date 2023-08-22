797ab735ce41a3df065ea7a26dd416bc1c9a3bc4
craygenx <marshmellowking99@gmail.com>
2023-08-22 19:54:25 +0300

    Completed assignment

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,10 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 0:
+        return ""
+    elif len(items) == 1:
+        return items[0]
+    elif len(items) == 2:
+        return f"{items[0]} and {items[1]}"
+    else:
+        # Join all elements except the last one with commas and add 'and' before the last element
+        return f"{', '.join(items[:-1])}, and {items[-1]}"
